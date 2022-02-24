dd66e5694bddaae007356a41893838848be1c808
Denikaev Emil <edenikaev@mail.ru>
2022-02-24 23:31:51 +0400

    Добавил регулярные выражения

--- js/script.js
+++ js/script.js
@@ -357,17 +357,21 @@ window.addEventListener('DOMContentLoaded', () => {
       current.textContent = slideIndex
     }
   }
-  
+
   function dotsOpacity() {
     dots.forEach(dot => dot.style.opacity = '.5');
     dots[slideIndex - 1].style.opacity = 1;
   }
 
+  function deleteNotDigits(str){
+    return +str.replace(/\D/g, '');
+  }
+
    next.addEventListener('click',  () => {
-    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+    if (offset == deleteNotDigits(width) * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += +width.slice(0, width.length - 2);
+      offset += deleteNotDigits(width);
     }
     slidesField.style.transform = `translateX(-${offset}px)`
 
@@ -382,9 +386,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   prev.addEventListener('click',  () => {
     if (offset == 0) {
-      offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+      offset = deleteNotDigits(width) * (slides.length - 1)
     } else {
-      offset -= +width.slice(0, width.length - 2);
+      offset -= deleteNotDigits(width);
     }
     slidesField.style.transform = `translateX(-${offset}px)`
 
@@ -401,7 +405,7 @@ window.addEventListener('DOMContentLoaded', () => {
     dot.addEventListener('click', (e) => {
       const slideTo = e.target.getAttribute('data-slide-to');
       slideIndex = slideTo;
-      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+      offset = deleteNotDigits(width) * (slideTo - 1);
       slidesField.style.transform = `translateX(-${offset}px)`;
       currentSlidesLegth();
       dotsOpacity();
